68cce51458e9626728b854c6803129350d803506
Vadzim Hanchuk <31338180+v-dz-m@users.noreply.github.com>
2025-01-10 15:55:58 +0300

    Add lesson 18

---
+++ local/templates/main/components/bitrix/news.detail/portfolio_detail/result_modifier.php
@@ -0,0 +1,7 @@
+<?php
+if (!empty($arResult['PROPERTIES']['gallery']['VALUE'])) {
+    foreach ($arResult['PROPERTIES']['gallery']['VALUE'] as $key => $photoId) {
+        $arPhoto = CFile::GetFileArray($photoId);
+        $arResult['PROPERTIES']['photos'][$key]['src'] = $arPhoto['SRC'];
+    }
+}
